bea0a84866e5bd019fb5fb5fc3066a7feb10868a
yaniv dadon <y2.71828@gmail.com>
2021-02-21 22:40:48 +0200

    add conf file

---
+++ boot.py
@@ -0,0 +1,31 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+import gc
+#import webrepl
+#webrepl.start()
+#gc.collect()
+import conf
+
+def do_connect(ssid, pwd):
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(ssid, pwd)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+ 
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+ 
+# Attempt to connect to WiFi network
+do_connect(ssid, pss)
+ 
+#import webrepl
+#webrepl.start()
